19822ee328ce5a8e20a1262bb1e4d849aa5e87e7
DILIP SANAP <Dilip Sanap>
2023-08-04 18:25:19 +0530

    User model and user route created

---
+++ Backend/Models/user.model.js
@@ -0,0 +1,26 @@
+const {DataTypes } = require('sequelize');
+const sequelize = require('../Config/Db');
+
+const User = sequelize.define('User', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    name: {
+        type: DataTypes.STRING(32),
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING(32),
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING(500),
+        allowNull: false
+    }
+});
+
+
+module.exports = User ;
---
+++ Backend/Routes/user.route.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const sequelize = require("../Config/Db");
+const User = require("../Models/user.model");
+
+const userRouter = express.Router();
+
+// Create the user
+
+userRouter.post("/api/register", async (req, res) => {
+  const { name, email, password } = req.body;
+  // console.log(req.body);
+  try {
+     const existingUser = await User.findOne({ where: { email } });
+
+     if (existingUser) {
+       return res
+         .status(409)
+         .json({ message: "User with this email already exists" });
+     }
+
+    bcrypt.hash(password, 5, async (err, hash) => {
+      if (err) {
+        console.log(err);
+        res
+          .status(500)
+          .json({ message: "Error occurred during password hashing" });
+      } else {
+        try {
+          const newUser = await User.create({ name, email, password: hash });
+          res.status(201).json({ message: "Registration is successful" });
+        } catch (err) {
+            res
+                .status(500).send({
+                    message: "Error occur at registering the use", "Error": err.message})
+        }
+      }
+    });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+
+module.exports = { userRouter };
