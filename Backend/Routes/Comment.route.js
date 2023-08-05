20c4cf849e29775a2127d13b8dbbd1888983802b
DILIP SANAP <Dilip Sanap>
2023-08-05 13:48:26 +0530

    delete a comment

--- Backend/Routes/Comment.route.js
+++ Backend/Routes/Comment.route.js
@@ -68,4 +68,31 @@ commentRouter.put('/api/comments/:commentId', authenticate, async (req, res) => 
     }
 });
 
+
+// delete a comment
+commentRouter.delete(
+    "/api/comments/:commentId",
+    authenticate,
+    async (req, res) => {
+        try {
+            const commentId = req.params.commentId;
+
+            const deletedComment = await Comment.destroy({
+                where: { id: commentId },
+            });
+            if (!deletedComment) {
+                return res.status(404).json({ message: "Comment not found" });
+            }
+            res.status(200).json({ message: "Comment deleted successfully" });
+        } catch (error) {
+            console.log(error);
+            res
+                .status(500)
+                .json({
+                    message: "Failed to delete the comment",
+                    error: error.message,
+                });
+        }
+    }
+);
 module.exports = commentRouter;
