f9d5c7e0316ed4e8b27622d26c60b52b0cda095f
DILIP SANAP <Dilip Sanap>
2023-08-05 19:53:05 +0530

    Backend models updated and conflicts resolved

--- Backend/Models/user.model.js
+++ Backend/Models/user.model.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define("users", {
     name: {
@@ -16,43 +16,4 @@ module.exports = (sequelize, DataTypes) => {
     },
   });
   return User;
-=======
-const { DataTypes } = require("sequelize");
-const sequelize = require("../Config/Db");
-
-const User = sequelize.define("User", {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-  },
-  name: {
-    type: DataTypes.STRING(32),
-    allowNull: false,
-  },
-  email: {
-    type: DataTypes.STRING(32),
-    allowNull: false,
-    unique: true,
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-});
-let isTableSynced = false;
-
-// function to check the table is synced orr not
-const syncTableOnce = async () => {
-  if (!isTableSynced) {
-    try {
-      await sequelize.sync();
-      console.log("Table created successfully");
-      isTableSynced = true;
-    } catch (error) {
-      console.log(error.message);
-      console.log("Failed to create table");
-    }
-  }
->>>>>>> main
 };
