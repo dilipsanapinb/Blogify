5c00f75c9f52b2092203c72559f1a9465940a721
DILIP SANAP <Dilip Sanap>
2023-08-07 19:22:32 +0530

    blogify comment route authorised

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,17 @@
 import './App.css';
 import {Route,BrowserRouter as Router,Routes} from "react-router-dom" 
 import LandingPage from './Pages/LandingPage';
+import SignInPage from './Pages/SignInPage';
+import BlogsPage from './Pages/BlogsPage';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<LandingPage/>}></Route>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/signin" element={<SignInPage />} />
+          <Route path="/blogs" element={<BlogsPage />} />
         </Routes>
       </Router>
     </div>
